f75cb3da7b238d32506060cfcf5102777a1e3140
MarionBenda <marionbenda@yahoo.de>
2026-03-21 09:04:45 +0100

    erster Teil db.js eingefügt inkl. render function, html und css für section menu eingefügt

--- scripts/db.js
+++ scripts/db.js
@@ -1,1 +1,25 @@
 //hier kommt das JSNON rein
+let MY_DISHES = [
+  {
+    image: './assets/img/burger_cheese_beef.jpg',
+    name: 'Classic Cheeseburger',
+    price: 9.5,
+    description: 'Saftiges Rindfleisch mit Cheddar, Tomaten und Haussauce.',
+  },
+  {
+    name: 'Chicken Burger',
+    price: 12.99,
+    description: 'Saftiges Hühnerfilet',
+  },
+  {
+    name: 'Double Burger',
+    price: 12.99,
+
+    description: 'fffff',
+  },
+  {
+    name: 'Spicy special Burger',
+    price: 12.99,
+    description: 'fff',
+  },
+];
---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function init() {
+  renderDishes();
+}
+
+function renderDishes() {
+  let dishesContainer = document.getElementById('dishes');
+  if (!dishesContainer) return;
+
+  dishesContainer.innerHTML = '';
+
+  MY_DISHES.forEach((burger) => {
+    dishesContainer.innerHTML += `
+      <div class="burger-card">
+    //   <img src="${burger.image}" alt="${burger.name}" class="burger-img">
+          <h3>${burger.name}</h3>
+          <p class="description">${burger.description}</p>
+          <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+      </div>
+    `;
+  });
+}
+
+// Manuelle Ausführung am Ende der Datei
+init();
