29664078dcf307f4052817992c3a5ee8e0521196
MarionBenda <marionbenda@yahoo.de>
2026-03-21 10:13:06 +0100

    Array alle Gerichte, Preise, Beschreibungen und Bilder eingefügt

--- scripts/db.js
+++ scripts/db.js
@@ -1,15 +1,70 @@
 //hier kommt das JSNON rein
 let MY_DISHES = [
   {
-    image: './assets/img/burger_cheese_beef.jpg',
+    image: './assets/img/burger/burger_cheese_beef.jpg',
     name: 'Classic Cheeseburger',
     price: 9.5,
     description: 'Saftiges Rindfleisch mit Cheddar, Tomaten und Haussauce.',
   },
   {
+    image: './assets/img/burger/burger_chicken.jpg',
     name: 'Chicken Burger',
     price: 12.99,
-    description: 'Saftiges Hühnerfilet',
+    description:
+      'Saftiges Hühnerfilet trifft würzige Haussauce, Gouda, Salat und knackige Tomaten',
+  },
+  {
+    image: './assets/img/burger/burger_double.png',
+    name: 'Double Burger',
+    price: 12.99,
+
+    description:
+      'Doppelter saftiger Genuss zwei Stück Pattys, Zwiebeln, Haussauce, Cheedar, Salat und Tomaten',
+  },
+  {
+    image: './assets/img/burger/burger_spicy.jpg',
+    name: 'Spicy special Burger',
+    price: 12.99,
+    description:
+      'Der Burger mit Pfiff, saftiges Rindfleisch mit Chilli verfeinert, Tomate, Ruccola,Chillicheedar. Extra freurig',
+  },
+  //Pizza
+  {
+    image: './assets/img/pizza/pizza_ham_corn.jpg',
+    name: 'Pizza Cardinale',
+    price: 11.99,
+    description:
+      'Saftiger Schinken, würzige Tomatensauce, mit Mais und Mozarella',
+  },
+  {
+    image: './assets/img/pizza/pizza_magaritha.jpg',
+    name: 'Pizza Magaritha',
+    price: 10.9,
+    description: 'Würzige Tomatensauce mit Morzarella',
+  },
+  {
+    image: './assets/img/pizza/pizza_olivia.jpg',
+    name: 'Pizza Olivia',
+    price: 11.99,
+    description: 'Tomatensauce, Oliven und Morzarella',
+  },
+  {
+    image: './assets/img/pizza/pizza_salami.jpg',
+    name: 'Pizza Salami',
+    price: 14.99,
+    description: 'geräucherte Salami, würzige Tomatensauce und Morzarella',
+  },
+  {
+    image: './assets/img/pizza/pizza_tomaten_morzarella.jpg',
+    name: 'Pizza Siziliana',
+    price: 10.99,
+    description:
+      'Tomatensauce, Morzarella, mit ganzen Cocktailtoamten, Basilikum und  mit Balsamico verfeinert',
+  },
+  {
+    name: 'Spicy special Burger',
+    price: 12.99,
+    description: 'fff',
   },
   {
     name: 'Double Burger',
@@ -21,5 +76,71 @@ let MY_DISHES = [
     name: 'Spicy special Burger',
     price: 12.99,
     description: 'fff',
+  },
+  //Salate
+  {
+    image: './assets/img/salad/chicken_salad.jpg',
+    name: 'Spicy special Burger',
+    price: 12.99,
+    description: 'fff',
+  },
+  {
+    image: './assets/img/salad/cucumber_salad.png',
+    name: 'Double Burger',
+    price: 12.99,
+
+    description: 'fffff',
+  },
+  {
+    image: './assets/img/salad/greek_salad.png',
+    name: 'Spicy special Burger',
+    price: 12.99,
+    description: 'fff',
+  },
+  //side dishes
+  {
+    image: './assets/img/side_dishes/bruscetta.jpg',
+    name: 'Bruscetta',
+    price: 9.9,
+    description: 'Knuspriges Baguett mit Olivenöl, Tomaten und Basilikum',
+  },
+  {
+    image: './assets/img/side_dishes/onion_rings.jpg',
+    name: 'Onion Rings',
+    price: 11.99,
+
+    description: 'Saftige Zwiebeln fritiert in würzigem Backteig',
+  },
+  {
+    image: './assets/img/side_dishes/pommes.jpg',
+    name: 'Pommes frites',
+    price: 8.9,
+    description: 'heimische Kartoffeln knustprig fritiert',
+  },
+  {
+    image: './assets/img/side_dishes/potatoes.jpg',
+    name: 'Kartoffeln',
+    price: 5.99,
+    description:
+      'Heimische Kartoffeln gekocht, in Öl und Knoblauch angebraten und mit Petersilie verfeinert',
+  },
+  //Desserts
+  {
+    image: './assets/img/desserts/brownie.png',
+    name: 'Brownie',
+    price: 6.99,
+    description: 'Saftiger Brownie mit dunkler Schokolade',
+  },
+  {
+    image: './assets/img/desserts/cheese_cake.png',
+    name: 'American Cheescake',
+    price: 7.9,
+    description: 'Mürbteig mit Frischkäsecreme und einem Hauch Zitrone',
+  },
+  {
+    image: './assets/img/desserts/cookies.jpg',
+    name: 'Cookies',
+    price: 5.99,
+    description: 'Saftige, crunchy Cookies mit Schokoladenflakes',
   },
 ];
