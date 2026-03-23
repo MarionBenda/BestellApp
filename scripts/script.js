768df4a1a30848f6653f4b6001ac7fba5ef8e96a
MarionBenda <marionbenda@yahoo.de>
2026-03-23 13:08:54 +0100

    array, html, css menu content angepasst, array umgeändert it Objekten, java Script functionen angepasst, Icons für Beilagen und Desserts eingefügt

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,124 @@
+const rating = 4.5;
+const count = 215;
+
+document.getElementById('ratingValue').textContent = rating.toFixed(1);
+document.getElementById('ratingCount').textContent = `(${count})`;
+
 function init() {
-  renderDishes();
+  renderBurger();
+  renderPizza();
+  renderSalad();
+  renderSideDishes();
+  renderDesserts();
 }
 
-function renderDishes() {
-  let dishesContainer = document.getElementById('dishes');
+function renderBurger() {
+  let dishesContainer = document.getElementById('burger');
   if (!dishesContainer) return;
 
   dishesContainer.innerHTML = '';
 
-  MY_DISHES.forEach((burger) => {
+  MY_DISHES_NEW.burger.forEach((burger) => {
     dishesContainer.innerHTML += `
       <div class="burger-card">
-   <img src="${burger.image}" alt="${burger.name}" class="burger-img">
-          <h3>${burger.name}</h3>
-          <p class="description">${burger.description}</p>
-          <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+          <img src="${burger.image}" alt="${burger.name}" class="burger_img">
+          
+          <div class="card-body">
+              <div class="card-header">
+                  <h3>${burger.name}</h3>
+                  <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+              </div>
+              <p class="menu_description">${burger.description}</p>
+          </div>
       </div>
     `;
   });
 }
+function renderPizza() {
+  let dishesContainer = document.getElementById('pizza');
+  if (!dishesContainer) return;
 
-// Manuelle Ausführung am Ende der Datei
-init();
+  dishesContainer.innerHTML = '';
+
+  MY_DISHES_NEW.pizza.forEach((burger) => {
+    dishesContainer.innerHTML += `
+      <div class="burger-card">
+          <img src="${burger.image}" alt="${burger.name}" class="burger_img">
+          
+          <div class="card-body">
+              <div class="card-header">
+                  <h3>${burger.name}</h3>
+                  <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+              </div>
+              <p class="menu_description">${burger.description}</p>
+          </div>
+      </div>
+    `;
+  });
+}
+function renderSalad() {
+  let dishesContainer = document.getElementById('salad');
+  if (!dishesContainer) return;
+
+  dishesContainer.innerHTML = '';
+
+  MY_DISHES_NEW.salad.forEach((burger) => {
+    dishesContainer.innerHTML += `
+      <div class="burger-card">
+          <img src="${burger.image}" alt="${burger.name}" class="burger_img">
+          
+          <div class="card-body">
+              <div class="card-header">
+                  <h3>${burger.name}</h3>
+                  <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+              </div>
+              <p class="menu_description">${burger.description}</p>
+          </div>
+      </div>
+    `;
+  });
+}
+function renderSideDishes() {
+  let dishesContainer = document.getElementById('sideDishes');
+  if (!dishesContainer) return;
+
+  dishesContainer.innerHTML = '';
+
+  MY_DISHES_NEW.sideDishes.forEach((burger) => {
+    dishesContainer.innerHTML += `
+      <div class="burger-card">
+          <img src="${burger.image}" alt="${burger.name}" class="burger_img">
+          
+          <div class="card-body">
+              <div class="card-header">
+                  <h3>${burger.name}</h3>
+                  <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+              </div>
+              <p class="menu_description">${burger.description}</p>
+          </div>
+      </div>
+    `;
+  });
+}
+function renderDesserts() {
+  let dishesContainer = document.getElementById('desserts');
+  if (!dishesContainer) return;
+
+  dishesContainer.innerHTML = '';
+
+  MY_DISHES_NEW.desserts.forEach((burger) => {
+    dishesContainer.innerHTML += `
+      <div class="burger-card">
+          <img src="${burger.image}" alt="${burger.name}" class="burger_img">
+          
+          <div class="card-body">
+              <div class="card-header">
+                  <h3>${burger.name}</h3>
+                  <p class="price">${burger.price.toFixed(2).replace('.', ',')} €</p>
+              </div>
+              <p class="menu_description">${burger.description}</p>
+          </div>
+      </div>
+    `;
+  });
+}
