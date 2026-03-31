571570cea5eab0a0434b274c077b50f03a276acf
MarionBenda <marionbenda@yahoo.de>
2026-03-31 07:59:24 +0200

    Array angepasst mit Img Beschreibungen

--- scripts/db.js
+++ scripts/db.js
@@ -2,12 +2,14 @@ const MY_DISHES_NEW = {
   burger: [
     {
       image: './assets/img/burger/burger_cheese_beef.jpg',
+      alt: 'Klassischer Beef Burger mit einem Rindfleischpatty, Käse, knackigem Salat im Brioche Bun',
       name: 'Classic Cheeseburger',
       price: 9.5,
       description: 'Saftiges Rindfleisch mit Cheddar, Tomaten und Haussauce.',
     },
     {
       image: './assets/img/burger/burger_chicken.jpg',
+      alt: 'Zartes Filetpatty vom Huhn mit knackigem Salat, geschmolzenem Gouda und Housesauce',
       name: 'Chicken Burger',
       price: 12.99,
       description:
@@ -16,6 +18,7 @@ const MY_DISHES_NEW = {
 
     {
       image: './assets/img/burger/burger_double.png',
+      alt: 'Burger mit 2 saftigen Pattys, geschmolzenem Käse und knackigem Salat',
       name: 'Double Burger',
       price: 12.99,
       description:
@@ -23,6 +26,7 @@ const MY_DISHES_NEW = {
     },
     {
       image: './assets/img/burger/burger_spicy.jpg',
+      alt: 'Ein saftiger Burger mit sehr viel Chili und geschmolzenem Käse',
       name: 'Spicy special Burger',
       price: 12.99,
       description:
@@ -33,6 +37,7 @@ const MY_DISHES_NEW = {
   pizza: [
     {
       image: './assets/img/pizza/pizza_ham_corn.jpg',
+      alt: 'Pizza aus dem Steinofen mit Mais,Schinken und Morzarella',
       name: 'Pizza Cardinale',
       price: 11.99,
       description:
@@ -40,24 +45,28 @@ const MY_DISHES_NEW = {
     },
     {
       image: './assets/img/pizza/pizza_margaritha.jpg',
+      alt: 'Pizza aus dem Steinofen mit Tomatensauce und Morzarella belegt',
       name: 'Pizza Magaritha',
       price: 10.9,
       description: 'Würzige Tomatensauce mit Morzarella',
     },
     {
       image: './assets/img/pizza/pizza_olivia.jpg',
+      alt: 'Pizza aus dem Steinofen mit Oliven und Morzarella belegt',
       name: 'Pizza Olivia',
       price: 11.99,
       description: 'Tomatensauce, Oliven und Morzarella',
     },
     {
       image: './assets/img/pizza/pizza_salami.jpg',
+      alt: 'Pizza aus dem Steinofen mit geräucherter Salami und Morzarella',
       name: 'Pizza Salami',
       price: 14.99,
       description: 'geräucherte Salami, würzige Tomatensauce und Morzarella',
     },
     {
       image: './assets/img/pizza/pizza_tomaten_morzarella.jpg',
+      alt: 'Pizza aus dem Steinofen belegt mit Tomate, Morzarella, Basilikum und mit Balsamico garniert',
       name: 'Pizza Siziliana',
       price: 10.99,
       description:
@@ -68,6 +77,7 @@ const MY_DISHES_NEW = {
   salad: [
     {
       image: './assets/img/salad/chicken_salad.jpg',
+      alt: 'Knackiger grüner Salat, garniert mit Hühnerstreifen, Parmesan und Tomaten',
       name: 'Chicken Salad',
       price: 12.99,
       description:
@@ -75,6 +85,7 @@ const MY_DISHES_NEW = {
     },
     {
       image: './assets/img/salad/cucumber_salad.png',
+      alt: 'Gurkenscheiben garniert mit Zwiebeln und Chili',
       name: 'Gurken Salat',
       price: 7.99,
       description:
@@ -82,6 +93,7 @@ const MY_DISHES_NEW = {
     },
     {
       image: './assets/img/salad/greek_salad.png',
+      alt: 'Knackiger Salat mit Oliven, Schafskäse und Zwiebeln garniert',
       name: 'Griechischer Salat',
       price: 10.99,
       description:
@@ -92,12 +104,14 @@ const MY_DISHES_NEW = {
   sideDishes: [
     {
       image: './assets/img/side_dishes/bruscetta.jpg',
+      alt: '3 Stück original italienische Bruscetta angeröstet und mit saftigen Tomaten und Basilikum garniert',
       name: 'Bruscetta',
       price: 9.9,
       description: 'Knuspriges Baguett mit Olivenöl, Tomaten und Basilikum',
     },
     {
       image: './assets/img/side_dishes/onion_rings.jpg',
+      alt: 'Teller mit Zwiebelringe goldbraun fritiert',
       name: 'Onion Rings',
       price: 11.99,
 
@@ -105,12 +119,14 @@ const MY_DISHES_NEW = {
     },
     {
       image: './assets/img/side_dishes/pommes.jpg',
+      alt: 'Klassische knusprige Pommes frites auf einem Teller angerichtet mit 2 Saucen',
       name: 'Pommes frites',
       price: 8.9,
       description: 'heimische Kartoffeln knustprig fritiert',
     },
     {
       image: './assets/img/side_dishes/potatoes.jpg',
+      alt: 'Ein Teller mit Kartoffeln verfeinert mit Butter, Knoblauch und Petersilie',
       name: 'Kartoffeln',
       price: 5.99,
       description:
@@ -121,18 +137,21 @@ const MY_DISHES_NEW = {
   desserts: [
     {
       image: './assets/img/desserts/brownie.png',
+      alt: 'Nahaufnahme eines saftigen Brownies mit Schokosauce und Himbeeren verziert',
       name: 'Brownie',
       price: 6.99,
       description: 'Saftiger Brownie mit dunkler Schokolade',
     },
     {
       image: './assets/img/desserts/cheese_cake.png',
+      alt: 'Nahaufnahme eines saftigen Käsekuchens mit Himbeeren und Puderzucker garniert',
       name: 'American Cheescake',
       price: 7.9,
       description: 'Mürbteig mit Frischkäsecreme und einem Hauch Zitrone',
     },
     {
       image: './assets/img/desserts/cookies.jpg',
+      alt: 'Nahaufnahme 5 übereinander gesapelten Cookies mit Schokostückchen',
       name: 'Cookies',
       price: 5.99,
       description: 'Saftige, crunchy Cookies mit Schokoladenflakes',
